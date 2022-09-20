bcc8faf4ae4047ba54c1cf6e7a7d91e69bea0193
Illia Kazmin <kazmin1c@gmail.com>
2022-09-20 15:20:26 +0200

    initial html and js files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { fetchCountries } from './fetchCountries';
 
 const refs = {
-  searchInput: document.querySelector('#search-box'),
+  formEl: document.querySelector('#search-form'),
+  galleryContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
+
+refs.formEl.addEventListener('submit', onSearchRequest);
+
+function onSearchRequest(evt) {
+  evt.preventDefault();
+
+  const searchQuery = evt.currentTarget.elements.searchQuery.value;
+}
 
 // Notiflix.Notify.failure('Oops, there is no country with that name');
 // Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
